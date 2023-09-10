a9f20740f4444d512a84b9a7c821025719a4ff50
Folafolu <ofolafolu@gmail.com>
2023-09-10 22:20:56 +0100

    url

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('api/', views.get_user_info, name='get_user_info'),
+    path('api', views.get_user_info, name='get_user_info'),
 ]
+
+
